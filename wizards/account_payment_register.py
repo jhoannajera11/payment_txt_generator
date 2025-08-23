3577489d03fa87d603ad386dd712012d08b2cff4
Jhoan Najera <jhoan.najera11r@gmail.com>
2025-08-23 13:33:32 -0700

    upd: payment_register

--- wizards/account_payment_register.py
+++ wizards/account_payment_register.py
@@ -26,7 +26,7 @@ class AccountPaymentRegister(models.TransientModel):
         'ACTINVER': 'PRUDE', 'INTERCAM BANCO': 'REGIO', 'BANCOPPEL': 'COPEL',
         'ABC CAPITAL': 'AMIGO', 'CONSUBANCO': 'FACIL', 'VOLKSWAGEN BANK': 'VOLKS',
         'CI BANCO': 'CONSU', 'BBASE': 'BBASE', 'BANKAOOL': 'AGROF', 'PAGATODO': 'PTODO',
-        'INMOBILIario': 'INMOB', 'DONDE': 'DONDE', 'BANCREA': 'BCREA', 'BANCO FINTERRA': 'FINTE',
+        'INMOBILIARIO': 'INMOB', 'DONDE': 'DONDE', 'BANCREA': 'BCREA', 'BANCO FINTERRA': 'FINTE',
         'ICBC': 'ICBCH', 'SABADELL': 'SABAD', 'SHINAN': 'SHINH', 'MIZUHO BANK': 'MISUO',
         'BANCO S3': 'BCOS3', 'MONEX CASA DE BOLSA': '90600', 'GBM CASA DE BOLSA': '90601',
         'MASARI CASA DE BOLSA': '90602', 'VALUE CASA DE BOLSA': '90605', 'ESTRUCTURADORES': '90606',
@@ -69,27 +69,42 @@ class AccountPaymentRegister(models.TransientModel):
         return ''.join(fields)
 
     def _format_spei_line(self, payment):
+        """Genera una línea para el archivo de Transferencias SPEI (Layout 3)."""
         partner = payment.partner_id
         bank_account, bank = self._get_partner_bank_info(partner)
+        
         bank_code_str = self.BANK_CODE_MAP.get((bank.name or '').upper(), '')
-        if len(bank_code_str) == 4: bank_code_str = self._pad_right(bank_code_str, 5)
-        fields = ['LTX07', self._pad_right('65501571237', 18), self._pad_right(bank_account.acc_number, 20), self._pad_right(bank_code_str, 5), self._pad_right(partner.name, 40), '0100', self._pad_left_zeros(int(payment.amount * 100), 18), '01001', self._pad_right(partner.x_spei_reference, 40), ' ' * 7, self._pad_right(partner.email, 40), '1', ' ' * 8]
+        if len(bank_code_str) == 4:
+            bank_code_str = self._pad_right(bank_code_str, 5)
+
+        # La cuenta CLABE ahora viene del campo específico de la localización mexicana
+        clabe_account = bank_account.l10n_mx_edi_clabe or bank_account.acc_number
+        if not clabe_account:
+             raise UserError(_("La cuenta bancaria del proveedor '%s' no tiene un número de cuenta o CLABE configurado.") % partner.name)
+
+        fields = [
+            'LTX05',  # <-- CAMBIO 1: Código de Layout corregido a LTX05.
+            self._pad_right('65501571237', 18),
+            self._pad_right(clabe_account, 20),  # <-- CAMBIO 2: Se usa el campo de la CLABE.
+            self._pad_right(bank_code_str, 5),
+            self._pad_right(partner.name, 40),
+            '0100',
+            self._pad_left_zeros(int(payment.amount * 100), 18),
+            '01001',
+            self._pad_right(partner.x_spei_reference, 40),
+            ' ' * 7,
+            self._pad_right(partner.email, 40),
+            '1',
+            ' ' * 8,
+        ]
         return ''.join(fields)
 
     def action_create_payments_and_generate_file(self):
-        """
-        Esta es la función que se llama desde el botón.
-        Ahora es la responsable de todo el flujo.
-        """
-        # 1. Llama a la función de bajo nivel de Odoo para crear los pagos.
-        # Esta función SIEMPRE devuelve los registros de pago creados.
         payments_to_process = self._create_payments()
 
         if not payments_to_process:
-            # Si no se crearon pagos, simplemente cierra la ventana.
             return {'type': 'ir.actions.act_window_close'}
         
-        # 2. Reutiliza la lógica de agrupación y generación de archivos
         confirming_payments = payments_to_process.filtered(lambda p: p.partner_id.x_confirming_key)
         transfer_payments = payments_to_process - confirming_payments
         
@@ -111,7 +126,6 @@ class AccountPaymentRegister(models.TransientModel):
         if not generated_files:
             return {'type': 'ir.actions.act_window_close'}
 
-        # 3. Prepara el archivo (o archivos) para la descarga
         if len(generated_files) == 1:
             filename, content = list(generated_files.items())[0]
             file_data = base64.b64encode(content.encode('utf-8'))
@@ -123,12 +137,12 @@ class AccountPaymentRegister(models.TransientModel):
             filename = 'paquete_de_pagos.zip'
             file_data = base64.b64encode(zip_buffer.getvalue())
 
-        # 4. Crea un adjunto y devuelve la acción de descarga
         attachment = self.env['ir.attachment'].create({
             'name': filename,
             'datas': file_data,
             'type': 'binary',
-            'mimetype': 'application/zip' if filename.endswith('.zip') else 'text/plain',
+            'res_model': self._name,
+            'res_id': self.id,
         })
         
         return {
